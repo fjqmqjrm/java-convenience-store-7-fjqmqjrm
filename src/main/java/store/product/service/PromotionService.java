53168017e5998531de07caa894a78c6d8633bcad
uniiii <126189239+fjqmqjrm@users.noreply.github.com>
2024-11-11 03:24:42 +0900

    refactor: 간단한 리턴문 리팩토링

--- src/main/java/store/product/service/PromotionService.java
+++ src/main/java/store/product/service/PromotionService.java
@@ -35,25 +35,19 @@ public class PromotionService {
 
     public int calculateFreeItems(int purchaseQuantity, Promotion promotion) {
         var currentDate = DateTimes.now().toLocalDate();
-        if (!promotion.isActive(currentDate)) {
-            return 0;
-        }
+        if (!promotion.isActive(currentDate)) { return 0; }
         int buyQuantity = promotion.getBuyQuantity();
         int getQuantity = promotion.getGetQuantity();
         int setsEligibleForFreeItem = purchaseQuantity / (buyQuantity + getQuantity);
         int freeItems = setsEligibleForFreeItem * getQuantity;
         int remainingQuantity = purchaseQuantity % (buyQuantity + getQuantity);
-        if (remainingQuantity >= buyQuantity) {
-            freeItems += getQuantity;
-        }
+        if (remainingQuantity >= buyQuantity) { freeItems += getQuantity; }
         return freeItems;
     }
 
     public boolean isEligibleForAdditionalPromotion(Product product, int quantity) {
         Promotion promotion = getActivePromotion(product);
-        if (promotion == null) {
-            return false;
-        }
+        if (promotion == null) { return false; }
         int buyQuantity = promotion.getBuyQuantity();
         int promotionSetQuantity = buyQuantity + promotion.getGetQuantity();
         int remainingQuantity = quantity - buyQuantity;
@@ -62,14 +56,10 @@ public class PromotionService {
 
     public int calculateNonPromotionQuantity(Product product, int quantity) {
         Promotion promotion = getActivePromotion(product);
-        if (promotion == null) {
-            return quantity;
-        }
+        if (promotion == null) { return quantity; }
         int promotionSetQuantity = promotion.getBuyQuantity() + promotion.getGetQuantity();
         int maxPromotionQuantity = product.getPromotionStock() / promotionSetQuantity * promotionSetQuantity;
-        if (quantity > maxPromotionQuantity) {
-            return quantity - maxPromotionQuantity;
-        }
+        if (quantity > maxPromotionQuantity) { return quantity - maxPromotionQuantity; }
         return 0;
     }
 }
