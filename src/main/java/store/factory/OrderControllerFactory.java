9f9c32621a2e68a2d6df8c73cf31ca48b00e950c
uniiii <126189239+fjqmqjrm@users.noreply.github.com>
2024-11-11 03:36:08 +0900

    refactor: OrderControllerFactory 메서드 분리

--- src/main/java/store/factory/OrderControllerFactory.java
+++ src/main/java/store/factory/OrderControllerFactory.java
@@ -21,22 +21,48 @@ import store.handler.ItemHandler;
 public class OrderControllerFactory {
 
     public static OrderController createController() {
-        ProductRepository productRepository = new ProductRepository();
-        PromotionRepository promotionRepository = new PromotionRepository();
-        OrderRepository orderRepository = new OrderRepository();
-        PromotionService promotionService = new PromotionService(promotionRepository);
+        ProductRepository productRepository = createProductRepository();
+        OrderRepository orderRepository = createOrderRepository();
+        PromotionService promotionService = createPromotionService();
         MembershipService membershipService = new MembershipService();
         StockService stockService = new StockService();
         OrderService orderService = new OrderService(productRepository, orderRepository, membershipService, promotionService, stockService);
-        DiscountService discountService = new DiscountService(membershipService);
-        ReceiptService receiptService = new ReceiptService();
+        return new OrderController(createShoppingSessionManager(orderService, productRepository, promotionService), createDiscountManager(membershipService),
+                createReceiptManager(), orderService, new InputView(), new OutputView()
+        );
+    }
+
+    private static ProductRepository createProductRepository() {
+        return new ProductRepository();
+    }
+
+    private static OrderRepository createOrderRepository() {
+        return new OrderRepository();
+    }
+
+    private static PromotionService createPromotionService() {
+        PromotionRepository promotionRepository = new PromotionRepository();
+        return new PromotionService(promotionRepository);
+    }
+
+    private static ShoppingSessionManager createShoppingSessionManager(OrderService orderService, ProductRepository productRepository, PromotionService promotionService) {
         InputView inputView = new InputView();
         OutputView outputView = new OutputView();
         InputParser inputParser = new InputParser();
         ItemHandler itemHandler = new ItemHandler(productRepository, promotionService, orderService, inputParser, inputView, outputView);
-        ShoppingSessionManager shoppingSessionManager = new ShoppingSessionManager(itemHandler, inputView, outputView, orderService);
-        DiscountManager discountManager = new DiscountManager(discountService, inputView, outputView);
-        ReceiptManager receiptManager = new ReceiptManager(receiptService, outputView);
-        return new OrderController(shoppingSessionManager, discountManager, receiptManager, orderService, inputView, outputView);
+        return new ShoppingSessionManager(itemHandler, inputView, outputView, orderService);
+    }
+
+    private static DiscountManager createDiscountManager(MembershipService membershipService) {
+        DiscountService discountService = new DiscountService(membershipService);
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        return new DiscountManager(discountService, inputView, outputView);
+    }
+
+    private static ReceiptManager createReceiptManager() {
+        ReceiptService receiptService = new ReceiptService();
+        OutputView outputView = new OutputView();
+        return new ReceiptManager(receiptService, outputView);
     }
 }
